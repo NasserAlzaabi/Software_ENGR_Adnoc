f881112fc0c07644f27a788e5251be8abd0b37c3
NasserAlzaabi <Nasseralzaabi03@gmail.com>
2023-11-29 00:18:42 +0400

    time updates

--- CV.py
+++ CV.py
@@ -1,5 +1,10 @@
 import cv2
 import numpy as np
+import time
+
+last_update = time.time()
+frequency = 10 #how often time updates (in seconds)
+tt = 0
 
 def is_object_near_grey(grey_contour, non_grey_contours, min_distance=30):
     grey_x, grey_y, _, _ = cv2.boundingRect(grey_contour)
@@ -25,8 +30,12 @@ upper_blue = np.array([130, 255, 255])
 lower_green = np.array([35, 100, 100])
 upper_green = np.array([85, 255, 255])
 
-lower_grey = np.array([0, 0, 80])
-upper_grey = np.array([180, 30, 180])
+lower_grey = np.array([0, 0, 60])
+upper_grey = np.array([180, 30, 140])
+
+green_time = 1
+blue_time = 2
+red_time = 4
 
 while True:
     ret, frame = camera.read()
@@ -68,20 +77,33 @@ while True:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             green_count += 1
 
+    grey_count = 0
     for grey_contour in contours_grey:
         if cv2.contourArea(grey_contour) > 100:
             x, y, w, h = cv2.boundingRect(grey_contour)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (128, 128, 128), 2)
-
+            grey_count += 1
             # Check if any non-grey object is close to the grey object
             if is_object_near_grey(grey_contour, contours_red + contours_blue + contours_green):
                 cv2.putText(frame, 'Near Non-Grey', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    
+    
+    total_count = red_count + blue_count + green_count
+    current_time = time.time()
+    if current_time - last_update >= frequency:
+        if (total_count <= grey_count or grey_count == 0):
+            tt = 0
+        else:
+            tt = ((red_count*red_time) + (blue_count*blue_time) + (green_count*green_time)) / grey_count
+        last_update = current_time
+    
 
     # Display # of each object type on the frame
     cv2.putText(frame, f'Red: {red_count}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
     cv2.putText(frame, f'Blue: {blue_count}', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
     cv2.putText(frame, f'Green: {green_count}', (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-    cv2.putText(frame, f'Grey: {len(contours_grey)}', (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+    cv2.putText(frame, f'Grey: {grey_count}', (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+    cv2.putText(frame, f'Time: {int(tt)} mins', (frame.shape[1] - 300, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
 
     cv2.imshow('frame', frame)
     
